76fc02e91ef85cb14fe2a441839f324ff7fc87d7
reosjan331 <reosjan331@gmail.com>
2023-09-21 16:29:29 +0900

    3주차 실습1
    .

---
+++ 3week/3week/소스.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char ch;
+
+	printf("문자 입력 : ");
+
+	scanf_s("%c", &ch);
+	printf("%d", ch);
+	printf("%c문자의 아스키 코드 값은 %d입니다.", ch, ch);
+	
+	return 0;
+}
+
+
+	
